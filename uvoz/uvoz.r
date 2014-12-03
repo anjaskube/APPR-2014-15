8fde06809335086e6fe78d893271720ceaf7e88a
Anja Skube <anja.skube@gmail.com>
2014-12-03 11:03:51 +0100

    Uvozila podatke

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,16 +1,21 @@
 # 2. faza: Uvoz podatkov
 
 # Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvoziDruzine <- function() {
-  return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
-                      row.names = 1,
-                      col.names = c("obcina", "en", "dva", "tri", "stiri"),
-                      fileEncoding = "Windows-1250"))
+#uvoziKnjige <- function() {
+# return(read.table("podatki/anpppodatki.csv", sep = ";", as.is = TRUE,
+#                      row.names = 1,
+#                      col.names = c("Naslov", "Pisatelj", "Jezik izvirnika", "Leto izdaje", "Prodaja","Spol","Vrsta","Film","St. knjig na pisatelja"),
+#                      fileEncoding = "Windows-1250"))
+#}
+
+uvoziKnjige <- function(){
+  return(read.csv2(file="podatki/anpppodatki.csv", header=TRUE, sep=";"))
 }
 
-# Zapišimo podatke v razpredelnico druzine.
-cat("Uvažam podatke o družinah...\n")
-druzine <- uvoziDruzine()
+
+# Zapišimo podatke v razpredelnico knjige.
+cat("Uvažam podatke o knjigah...\n")
+knjige <- uvoziKnjige()
 
 # Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
 # potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
