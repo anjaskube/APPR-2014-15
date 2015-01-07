0d5b83689f6f8dfded03cab378b24991259f3fde
Anja Skube <anja.skube@gmail.com>
2015-01-07 12:06:36 +0100

    dodala sem grafe, popravila uvoz, naredila novo tabelo, v kateri sem dodala novi stolpec(drzava) in narisala zemljevid na katerem sem obarvala drzave glede na stevilo knjig v posamezni drzavi in dodala se legendo

--- slike/README.md
+++ slike/README.md
@@ -1,4 +1,20 @@
 # Mapa s slikami
 
-V to mapo bomo dajali vse slike (grafe, zemljevide), ki jih bo naš program
-naredil in jih bomo vključili v končno poročilo.
+#V to mapo bomo dajali vse slike (grafe, zemljevide), ki jih bo naš program
+#naredil in jih bomo vključili v končno poročilo.
+
+
+pdf("slike/grafi.pdf",paper="a4")
+tabela<-table(KNJIGE$Vrsta)
+imena<-names(tabela)
+vektor<-c(tabela)
+barplot(vektor, names.arg = imena, xlab="VRSTA", ylab="ŠTEVILO KNJIG", main= 'ŠTEVILO KNJIG PO VRSTAH', las=2, cex.names=0.75, col="red")
+dev.off()
+
+pdf("slike/graf1.pdf",paper="a4")
+tab<-table(KNJIGE$Spol)
+vekto<-c(tab)
+barplot(vekto, names.arg = c("Moski", "Zenske"), xlab="SPOL", ylab="ŠTEVILO", main= 'PISATELJI PO SPOLU', las=2, cex.names=0.75, col="red")
+dev.off()
+
+
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -4,43 +4,61 @@
 source("lib/uvozi.zemljevid.r")
 
 # Uvozimo zemljevid.
-cat("Uvažam zemljevid...\n")
-obcine <- uvozi.zemljevid("http://e-prostor.gov.si/fileadmin/BREZPLACNI_POD/RPE/OB.zip",
-                          "obcine", "OB/OB.shp", mapa = "zemljevid",
-                          encoding = "Windows-1250")
+cat("Uvažam zemljevid sveta...\n")
+svet <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_map_units.zip",
+                        "svet", "ne_110m_admin_0_map_units.shp", mapa = "zemljevid",
+                        encoding = "Windows-1252")
 
-# Funkcija, ki podatke preuredi glede na vrstni red v zemljevidu
-preuredi <- function(podatki, zemljevid) {
-  nove.obcine <- c()
-  manjkajo <- ! nove.obcine %in% rownames(podatki)
-  M <- as.data.frame(matrix(nrow=sum(manjkajo), ncol=length(podatki)))
-  names(M) <- names(podatki)
-  row.names(M) <- nove.obcine[manjkajo]
-  podatki <- rbind(podatki, M)
-  
-  out <- data.frame(podatki[order(rownames(podatki)), ])[rank(levels(zemljevid$OB_UIME)[rank(zemljevid$OB_UIME)]), ]
-  if (ncol(podatki) == 1) {
-    out <- data.frame(out)
-    names(out) <- names(podatki)
-    rownames(out) <- rownames(podatki)
-  }
-  return(out)
-}
+svet<- svet[svet$continent %in% c("Europe","Asia","South America","North America") | svet$name_long == "Australia",]
 
-# Preuredimo podatke, da jih bomo lahko izrisali na zemljevid.
-druzine <- preuredi(druzine, obcine)
+drzave <- table(KNJIGE$Drzava)
+stevilo <- unique(drzave)
+stevilo <- stevilo[order(stevilo)]
+barve <- topo.colors(length(stevilo))[match(drzave, stevilo)]
+names(barve) <- names(drzave)
+barve.zemljevid <- barve[as.character(svet$name_long)]
+barve.zemljevid[is.na(barve.zemljevid)] <- "white"
 
-# Izračunamo povprečno velikost družine.
-druzine$povprecje <- apply(druzine[1:4], 1, function(x) sum(x*(1:4))/sum(x))
-min.povprecje <- min(druzine$povprecje, na.rm=TRUE)
-max.povprecje <- max(druzine$povprecje, na.rm=TRUE)
-
-# Narišimo zemljevid v PDF.
-cat("Rišem zemljevid...\n")
-pdf("slike/povprecna_druzina.pdf", width=6, height=4)
-
-n = 100
-barve = topo.colors(n)[1+(n-1)*(druzine$povprecje-min.povprecje)/(max.povprecje-min.povprecje)]
-plot(obcine, col = barve)
-
+pdf("slike/knjige.pdf", width=8.27, height=11.69)
+plot(svet, col=barve.zemljevid, bg="lightblue")
+legend("topleft", title = 'število knjig po državah', text.font = 2,legend = stevilo, fill = topo.colors(length(stevilo)))
 dev.off()
+
+
+# # Funkcija, ki podatke preuredi glede na vrstni red v zemljevidu
+# preuredi <- function(podatki, zemljevid) {
+#   nove.svet <- c()
+#   manjkajo <- ! nove.obcine %in% rownames(podatki)
+#   M <- as.data.frame(matrix(nrow=sum(manjkajo), ncol=length(podatki)))
+#   names(M) <- names(podatki)
+#   row.names(M) <- nove.obcine[manjkajo]
+#   podatki <- rbind(podatki, M)
+#   
+#   out <- data.frame(podatki[order(rownames(podatki)), ])[rank(levels(zemljevid$OB_UIME)[rank(zemljevid$OB_UIME)]), ]
+#   if (ncol(podatki) == 1) {
+#     out <- data.frame(out)
+#     names(out) <- names(podatki)
+#     rownames(out) <- rownames(podatki)
+#   }
+#   return(out)
+# }
+# 
+# # Preuredimo podatke, da jih bomo lahko izrisali na zemljevid.
+# drzave <- preuredi(drzave, svet)
+# 
+# 
+# # Narišimo zemljevid v PDF.
+# cat("Rišem zemljevid sveta...\n")
+# pdf("slike/svet.pdf")
+# 
+# #n = 100
+# #barve = topo.colors(n)[1+(n-1)*(druzine$povprecje-min.povprecje)/(max.povprecje-min.povprecje)]
+# #plot(obcine, col = barve)
+# 
+# n = 100
+# barve =rgb(1, 1, (n:1)/n)[unlist(1+(n-1)*norm.povprecje)]
+# plot(slo, col = barve,bg="lightblue")
+# text(koordinate1,labels=imena1,cex=0.4)
+# title("Povprečna poraba vode na prebivalca")
+# 
+# dev.off()
