5cc15e4f6bbaed4dcefb55713c39a4cd4b2ba015
Anja Skube <anja.skube@gmail.com>
2015-01-13 14:12:23 +0100

    Popravila sem napake, ki so bile zapisane v "Issues".

--- projekt.r
+++ projekt.r
@@ -3,7 +3,7 @@
 # source("clearpdf.r", encoding = "UTF-8")
 # # 2. faza: Obdelava, uvoz in čiščenje podatkov
 source("uvoz/uvoz.r", encoding="UTF-8")
-#source("slike/grafi.r", encoding="UTF-8")
+source("slike/grafi.r", encoding="UTF-8")
 # #3. faza: Analiza in vizualizacija podatkov
 source("vizualizacija/vizualizacija.r",encoding="UTF-8")
 # # 4. faza: Napredna analiza podatkov
@@ -19,7 +19,7 @@ source("vizualizacija/vizualizacija.r",encoding="UTF-8")
 # cat("Končano.\n")
 
 # 2. faza: Obdelava, uvoz in čiščenje podatkov
-source("uvoz/uvoz.r", encoding="UTF-8")
+#source("uvoz/uvoz.r", encoding="UTF-8")
 #source("slike/grafi.r", encoding="UTF-8")
 
 # 3. faza: Analiza in vizualizacija podatkov
